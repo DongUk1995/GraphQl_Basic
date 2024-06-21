8589db3d98fd99750a749f158209760f51d90c26
김동욱 <email@example.com>
2024-06-21 14:37:42 +0900

    end

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# GraphQl_Basic
+1. 이 프로젝트를 만드는 목적
+
+- 기초공부
+- 내가 리모콘을 만드는 것 -> api를 만드는 건데 api는 인터 페이스 잖아 그럼 내가 만들면 뭐 영화 등 데이터가 있는 api가 만들어 질 것이고 이 api가 사용자가 필요하면 나의 api를 가지고 사용 하는 것이다. 그러므로 내가 만든 api는 리모콘이고 나는 리모콘 안에 버튼들을 만들어 필요한 사용자에게 데이터를 제공한다. 그리고 그것을 표현 할 수 있는 것이 아폴로 서버라는 것이다.
+
+2.  root에 대해
+
+- 아폴로 사용자 요청 : {alltweets {id, text, author {fullname}}}으로 호출했음
+  1. alltweets는 tweets를 리턴하는데 tweetssms id, text, userId가 있지만 author 없고 Fullname도 없음
+  2. 그 후 아폴로는 resolvers에 author를 찾아 유저아이디로 id와 같은 것을 출력하여
+
+  3. resolvers에 유저안에 fullname을 찾을거고 fullname을 호출할 것이다.
--- server.js
+++ server.js
@@ -1,13 +1,95 @@
 import { ApolloServer, gql } from "apollo-server"; // type : module을 사용하는 이유
 
+let tweets = [
+  {
+    id: "1",
+    text: "hello",
+    userId: "2",
+  },
+  { id: "2", text: "hello", userId: "1" },
+];
+
+let users = [
+  {
+    id: "1",
+    firstName: "kim",
+    lastName: "donguk",
+  },
+  {
+    id: "2",
+    firstName: "Elon",
+    lastName: "Musk",
+  },
+];
 const typeDefs = gql`
+  type User {
+    id: ID!
+    firstName: String
+    lastName: String!
+    fullName: String!
+  }
+
+  type Tweet {
+    id: ID!
+    text: String!
+    author: User
+  }
   type Query {
-    text: String
-    hello: String
+    allUsers: [User!]!
+    allTweets: [Tweet!]!
+    tweet(id: ID!): Tweet
+  }
+  type Mutation {
+    postTweet(text: String!, userId: ID!): Tweet!
+    deleteTweet(id: ID!): Boolean
   }
 `; // Graphql 한테 우리의 데이터의 모양을 설명해줌
+// POST한 유저에게 userId를 받아온다.
 
-const server = new ApolloServer({ typeDefs });
+const resolvers = {
+  Query: {
+    allTweets() {
+      return tweets;
+    },
+    tweet(root, { id }) {
+      return tweets.find((tweet) => tweet.id === id);
+    },
+    allUsers() {
+      return users;
+    },
+  },
+  Mutation: {
+    postTweet(_, { text, userId }) {
+      const newTweet = {
+        id: tweets.length + 1,
+        text,
+        userId,
+      };
+      tweets.push(newTweet);
+      return newTweet;
+    },
+    deleteTweet(_, { id }) {
+      const tweet = tweets.find((tweet) => tweet.id === id);
+      if (!tweet) return false;
+      tweets = tweets.filter((tweet) => tweet.id !== id);
+      return true;
+    },
+  },
+  User: {
+    fullName({ fristName, lastName }) {
+      console.log("dddd");
+      return `${fristName}${lastName}`;
+    },
+  },
+  Tweet: {
+    author({ userId }) {
+      console.log("Aaaa");
+      return users.find((user) => user.id == userId);
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
 server.listen().then(({ url }) => {
   console.log(`Running on ${url}`);
 });
